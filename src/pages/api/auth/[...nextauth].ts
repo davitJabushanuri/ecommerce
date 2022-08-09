6448b9d52b3fec6d11c9af8b01ebfa2a022dd8ff
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-08-09 22:58:30 +0400

    configure prisma schema for auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,13 @@
 import NextAuth from 'next-auth/next'
 import GithubProvider from 'next-auth/providers/github'
 
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID || '',
@@ -11,7 +17,4 @@ export default NextAuth({
 
   debug: process.env.NODE_ENV === 'development',
   secret: process.env.AUTH_SECRET,
-  jwt: {
-    secret: process.env.JWT_SECRET,
-  },
 })

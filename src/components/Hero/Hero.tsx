e38eb07fc2b8c8e4b8d40e0f91bd704078602b47
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-08-15 19:47:13 +0400

    create hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,7 +1,16 @@
 import styles from './Hero.module.scss'
+import { BsArrowRight } from 'react-icons/bs'
 
 const Hero = () => {
-  return <div className={styles.container}>Enter</div>
+  return (
+    <div className={styles.container}>
+      <h1>Shop fresh adidas back-to-school looks</h1>
+      <p>Score deals on must have 3-Stripes first day outfits</p>
+      <button>
+        Stay cool <BsArrowRight />
+      </button>
+    </div>
+  )
 }
 
 export default Hero

b2818f151cbfd1cca70b582c2526810e3ed11b13
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-08-17 04:13:02 +0400

    implement react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,29 @@ import 'swiper/css/bundle'
 import type { AppProps } from 'next/app'
 import { ChakraProvider, theme } from '@chakra-ui/react'
 import { SessionProvider } from 'next-auth/react'
+import { useState } from 'react'
+
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient())
+
   return (
-    <ChakraProvider theme={theme} resetCSS={true}>
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Hydrate state={pageProps.dehydratedState}>
+        <ChakraProvider theme={theme} resetCSS={true}>
+          <SessionProvider session={session}>
+            <Component {...pageProps} />
+          </SessionProvider>
+        </ChakraProvider>
+      </Hydrate>
+    </QueryClientProvider>
   )
 }
 

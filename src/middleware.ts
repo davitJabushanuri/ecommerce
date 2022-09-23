34f7a305bced34de67a1bb23327f37e3c9c075ba
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-09-24 02:09:28 +0400

    fix deployment errors

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,11 +3,4 @@ import type { NextRequest } from 'next/server'
 import * as jwt from 'next-auth/jwt'
 import { getSession } from 'next-auth/react'
 
-export default async function middleware(req: NextRequest) {
-  const secret = process.env.JWT_SECRET
-  const session = await getSession()
-  // Redirect from login page if already logged in
-  if (session && req.url.includes('/signin')) {
-    return NextResponse.redirect(new URL('/', req.url))
-  }
-}
+export default async function middleware(req: NextRequest) {}
--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -2,15 +2,11 @@ import styles from './[slug].module.scss'
 
 import Footer from 'components/Footer/Footer'
 import Header from 'components/Header/Header'
-import useProductStore from '../../components/store/productStore'
 
 import { IProduct } from 'ts/interfaces/IProduct'
 import Card from 'components/Card/Card'
-import { useQuery, useQueryClient } from '@tanstack/react-query'
-import { useEffect, useState } from 'react'
-import fetchPaginatedProducts from 'components/helpers/fetchPaginatedProducts'
+import { useQuery } from '@tanstack/react-query'
 
-import { useRouter } from 'next/router'
 import fetchProducts from 'components/helpers/fetchProducts'
 
 const Products = () => {

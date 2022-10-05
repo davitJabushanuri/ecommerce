f9f5e7b5ddd6ac76a7810a109dfcb211bae5698b
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-10-05 23:25:56 +0400

    refactor report modal

--- src/components/Report/Report.tsx
+++ src/components/Report/Report.tsx
@@ -1,7 +1,10 @@
 import createReport from '@components/helpers/createReport'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useFormik } from 'formik'
+import { useEffect } from 'react'
 import styles from './Report.module.scss'
+
+import { MdOutlineClose } from 'react-icons/md'
 
 const Report = ({ toggleModal, review }: any) => {
   const queryClient = useQueryClient()
@@ -33,11 +36,22 @@ const Report = ({ toggleModal, review }: any) => {
     }
   )
 
+  useEffect(() => {
+    document.body.style.overflowY = 'hidden'
+    return () => {
+      document.body.style.overflowY = 'auto'
+    }
+  }, [])
+
   return (
     <div onClick={() => toggleModal(false)} className={styles.container}>
       <div onClick={(e) => e.stopPropagation()} className={styles.modal}>
-        <button onClick={() => toggleModal(false)}>cancel</button>
-        <h1>Report</h1>
+        <div className={styles.header}>
+          <h2>Create Review</h2>
+          <button onClick={() => toggleModal(false)}>
+            <MdOutlineClose />
+          </button>
+        </div>
         <p>
           If you find this content inappropriate and think it should be removed
           from the our site, let us know by clicking the button below. feel free
@@ -45,34 +59,44 @@ const Report = ({ toggleModal, review }: any) => {
         </p>
 
         <form onSubmit={formik.handleSubmit}>
-          <label htmlFor="message">Message (optional)</label>
-          <input
-            type="text"
-            name="message"
-            value={formik.values.message}
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-          />
+          <div className={styles.inputGroup}>
+            <label htmlFor="message">
+              Message <span>(optional)</span>
+            </label>
+            <input
+              type="text"
+              name="message"
+              value={formik.values.message}
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+            />
+          </div>
 
-          <label htmlFor="description">Description (optional)</label>
-          <textarea
-            id="description"
-            name="description"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.description}
-            placeholder="Provide additional details"
-          />
+          <div className={styles.inputGroup}>
+            <label htmlFor="description">
+              Description <span>(optional)</span>
+            </label>
+            <textarea
+              id="description"
+              name="description"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.description}
+              placeholder="Provide additional details"
+            />
+          </div>
 
-          <button type="submit" disabled={mutation.isLoading}>
-            {mutation.isLoading
-              ? 'Saving...'
-              : mutation.isError
-              ? 'Error!'
-              : mutation.isSuccess
-              ? 'Saved!'
-              : 'Submit'}
-          </button>
+          <div className={styles.buttonContainer}>
+            <button type="submit" disabled={mutation.isLoading}>
+              {mutation.isLoading
+                ? 'Saving...'
+                : mutation.isError
+                ? 'Error!'
+                : mutation.isSuccess
+                ? 'Saved!'
+                : 'Submit'}
+            </button>
+          </div>
         </form>
       </div>
     </div>

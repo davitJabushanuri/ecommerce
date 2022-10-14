16f8f34602c67ba4b397c910ba84383bbcaa63f8
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-10-14 19:18:39 +0400

    style cartItem component

--- src/components/Cart/CartItem.tsx
+++ src/components/Cart/CartItem.tsx
@@ -4,6 +4,8 @@ import { IProduct } from '@ts/interfaces/types'
 import { useState } from 'react'
 import styles from './CartItem.module.scss'
 
+import { FaTrash } from 'react-icons/fa'
+
 interface CartItemProps {
   userId: string
   productId: string
@@ -29,39 +31,59 @@ const CartItem: React.FC<CartItemProps> = ({
 
       <div className={styles.info}>
         <h2>{product?.name}</h2>
-        <p>${product?.price}</p>
+        <p className={styles.price}>${product?.price}</p>
         <p>
           {product?.shipping === 0
             ? `Free shipping`
             : product?.shipping + ' ' + '$'}
         </p>
-        <div className={styles.inStock}>
+        <div className={product.stock > 0 ? styles.inStock : styles.outOfStock}>
           <p>{product.stock === 0 ? 'Out of stock' : 'In stock'}</p>
         </div>
 
-        <div className={styles.quantity}>
-          <button>-</button>
-          <input
-            type="text"
-            value={quantityState}
-            onChange={(e) => setQuantity(Number(e.target.value))}
-          />
-          <button>+</button>
+        <div className={styles.deleteContainer}>
+          <div className={styles.quantity}>
+            <button
+              className={styles.minus}
+              disabled={quantityState === 1}
+              onClick={() => {
+                setQuantity((prevQuantity) => prevQuantity - 1)
+              }}
+            >
+              -
+            </button>
+            <input
+              type="text"
+              value={quantityState}
+              onChange={(e) => setQuantity(Number(e.target.value))}
+            />
+            <button
+              className={styles.plus}
+              disabled={quantityState >= product?.stock}
+              onClick={() => {
+                setQuantity((prevQuantity) => prevQuantity + 1)
+              }}
+            >
+              +
+            </button>
+          </div>
+          <button
+            className={styles.delete}
+            disabled={cartMutation.isLoading}
+            onClick={() =>
+              cartMutation.mutate({
+                userId: userId,
+                productId: productId,
+                quantity: '1',
+              })
+            }
+          >
+            <span>
+              <FaTrash />
+            </span>
+            <p>Delete</p>
+          </button>
         </div>
-
-        <button
-          className={styles.delete}
-          disabled={cartMutation.isLoading}
-          onClick={() =>
-            cartMutation.mutate({
-              userId: userId,
-              productId: productId,
-              quantity: '1',
-            })
-          }
-        >
-          Delete
-        </button>
       </div>
     </div>
   )
